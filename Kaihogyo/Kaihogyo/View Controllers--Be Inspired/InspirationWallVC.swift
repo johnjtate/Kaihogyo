94cdaf487ac597d6025e215a6dd0e88a2357a5aa
John Tate <johnjtate@gmail.com>
2018-11-05 11:55:59 -0700

    creates skeleton of all BeInspired VCs

---
+++ Kaihogyo/Kaihogyo/View Controllers--Be Inspired/InspirationWallVC.swift
@@ -0,0 +1,30 @@
+//
+//  InspirationWallVC.swift
+//  Kaihogyo
+//
+//  Created by John Tate on 11/5/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import UIKit
+
+class InspirationWallVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
